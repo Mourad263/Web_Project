477b15828f4707cd7f6d09b8a35f93b479f0327f
nairaalhosini <96581428+nairaalhosini@users.noreply.github.com>
2024-04-19 20:14:50 +0200

    Add files via upload

--- storage.js
+++ storage.js
@@ -1,44 +1,35 @@
-function Book(ID, Name, author, Category, Description) {
-    this.ID = ID;
-    this.Name = Name;
-    this.author = author;
-    this.Category = Category;
-    this.Description = Description;
+class Book {
+    constructor(ID, Name, author, Category, Description) {
+        this.ID = ID;
+        this.Name = Name;
+        this.author = author;
+        this.Category = Category;
+        this.Description = Description;
+    }
 }
-var library = [];
+
+const library = [];
+
 function addBook() {
-    var ID = document.getElementById("id").value;
-    var Name = document.getElementById("bname").value;
-    var author = document.getElementById("ba").value;
-    var Category = document.getElementById("category").value;
-    var Description = document.getElementById("des").value;
-    var book = new Book(ID, Name, author, Category, Description);
+    const ID = document.getElementById("id").value;
+    const Name = document.getElementById("bname").value;
+    const author = document.getElementById("ba").value;
+    const Category = document.getElementById("category").value;
+    const Description = document.getElementById("des").value;
+    const book = new Book(ID, Name, author, Category, Description);
     library.push(book);
     displayBooks();
 }
-function displayBooks(books) {
-    var bookList = document.getElementById("bookList");
-    bookList.innerHTML = "";
-    if (!books) {
-        books = library;
-    }
-    for (var i = 0; i < books.length; i++) {
-        var tr = document.createElement("tr");
-        var tdID = document.createElement("td");
-        tdID.innerText = books[i].ID;
-        var tdName = document.createElement("td");
-        tdName.innerText = books[i].Name;
-        var tdAuthor = document.createElement("td");
-        tdAuthor.innerText = books[i].author;
-        var tdCategory = document.createElement("td");
-        tdCategory.innerText = books[i].Category;
-        var tdDescription = document.createElement("td");
-        tdDescription.innerText = books[i].Description;
-        tr.appendChild(tdID);
-        tr.appendChild(tdName);
-        tr.appendChild(tdAuthor);
-        tr.appendChild(tdCategory);
-        tr.appendChild(tdDescription);
-        bookList.appendChild(tr);
-    }
+
+function displayBooks(books = library) {
+    const bookList = document.getElementById("bookList");
+    bookList.innerHTML = books.map(book => `
+        <tr>
+            <td>${book.ID}</td>
+            <td>${book.Name}</td>
+            <td>${book.author}</td>
+            <td>${book.Category}</td>
+            <td>${book.Description}</td>
+        </tr>
+    `).join("");
 }
